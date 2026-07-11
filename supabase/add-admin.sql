90c49b48abc38a7649ceacd7ec1170f593a454cc
maker-soft <gazizov79@gmail.com>
2026-07-11 23:04:19 +0700

    Add files via upload

--- supabase/add-admin.sql
+++ supabase/add-admin.sql
@@ -1,4 +1,4 @@
 -- Замените значение на UUID пользователя из Supabase → Authentication → Users
 insert into public.admin_users(user_id)
-values ('304b8315-8663-44c2-b4ff-8b4f79665270')
+values ('2958443e-66ea-431c-8eba-581a260177e6')
 on conflict (user_id) do nothing;
